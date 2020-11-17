5b7209917256398deddee3889dc2eb22c9632e9d
Idan Amati <idan.amati@gmail.com>
2020-11-17 18:38:43 +0200

    better footer style

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -10,7 +10,7 @@ class Footer {
   build() {
     this.footer = document.createElement('footer');
     this.footer.classList.add('footer');
-    this.footer.innerHTML = `Created with Vanilla Js, Sass and Chart.js\t${percentMoji(Math.random(), 1)}\t<b>Idan Amati</b> at &lt;ITC&gt; Full Stack Bootcamp 2020`;
+    this.footer.innerHTML = `Created with Vanilla Js, Scss and Chart.js\t${percentMoji(Math.random(), 1)}\tby Idan Amati at &lt;ITC&gt; Full Stack Bootcamp 2020`;
     this.el.append(this.footer);
   }
 }
