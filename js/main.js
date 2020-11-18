f81039a92e20e8a6e0d6ed4f7e7f0b1a0417f0ed
Idan Amati <idan.amati@gmail.com>
2020-11-18 11:32:24 +0200

    removed unnecessary encapsulment

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@ import Marquee from './components/Marquee.js';
 import SearchBar from './components/SearchBar.js';
 import Footer from './components/Footer.js';
 
-(() => {
-  const marquee = new Marquee(document.querySelector('.marquee-wrapper'));
-  const searchBar = new SearchBar(document.querySelector('.search-bar'));
-  const footerContainer = document.createElement('div');
-  document.body.append(footerContainer);
-  const footer = new Footer(footerContainer);
-})();
+const marquee = new Marquee(document.querySelector('.marquee-wrapper'));
+const searchBar = new SearchBar(document.querySelector('.search-bar'));
+const footerContainer = document.createElement('div');
+document.body.append(footerContainer);
+const footer = new Footer(footerContainer);
